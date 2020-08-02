0f5b9aa4066abd7714fb0ef55d9304b63370204c
Welington Fidelis <welington.fidelis@tech4h.com.br>
2020-08-02 20:42:53 -0300

    Inclusão de comandos para migration e seeds; Seed de usuário melhorado

--- api/src/database/seeds/seed_users.js
+++ api/src/database/seeds/seed_users.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt');
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
@@ -8,7 +9,7 @@ exports.seed = function(knex) {
         { 
           name: 'usuario1', 
           login: 'usuario1', 
-          password: '$2b$10$P4iIftMu6WzzeuBdvGpti.B54CakNBo0/82yt7hPU5q1K8ep0iwPi' 
+          password: bcrypt.hashSync('1234', 10) 
         },
       ]);
     });
